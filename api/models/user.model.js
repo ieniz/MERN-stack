b7914a1cb84b6e9c9a855bf917713d984ef7af04
eniz <icanoviceniz@gmail.com>
2024-01-27 23:16:30 +0100

    testing api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema= new mongoose.Schema({
     username:{
         type: String,
         required: true,
         unique:true,
-    }
-    email {
+    },
+    email: {
         type: String,
         required: true,
         unique:true,
-    }
-    password {
+    },
+    password: {
         type: String,
         required: true,
         
