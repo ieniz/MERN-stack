0101e3323e078c24f046cd27f5ef76c35fac2e68
eniz <icanoviceniz@gmail.com>
2024-02-02 17:19:09 +0100

    Sign up correction and sign-in api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,8 @@ return <BrowserRouter>
 <Header/>
 <Routes>
 <Route path='/' element={<Home/>}/>
-<Route path='/signin' element={<SignIn/>} />
-<Route path='/signup' element={<SignUp/>} />
+<Route path='/sign-in' element={<SignIn/>} />
+<Route path='/sign-up' element={<SignUp/>} />
 <Route path='/about' element={<About/>} />
 <Route path='/profile' element={<Profile/>} />
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,19 +2,16 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
-
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -35,13 +32,12 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate('/signin');
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(error.message);
     }
   };
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
@@ -77,11 +73,11 @@ export default function SignUp() {
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={'/signin'}>
-          <span className='text-blue-600'>Sign in</span>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {error && <p className='text-red-400 mt-5'>{error}</p>}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
